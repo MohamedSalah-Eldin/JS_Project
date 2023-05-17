d9e8e13b9ec784ef0aae31bbb46729be53168ad5
MohamedSalah-Eldin <Mhmdabdelkhaliq12@gmail.com>
2023-05-17 12:09:24 +0300

    javascript testing

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -2,10 +2,16 @@ let usrname=document.getElementById("usernameId");
 let psswod=document.getElementById("passwordId");
 let mail=document.getElementById("emailId");
 let signup_BTN=document.getElementById("signupBTN");
+let login_BTN=document.getElementById("loginBTN");
+let psswodLogin=document.getElementById("passwordLogin_id");
+let mailLogin=document.getElementById("EmailLogin_Id");
 let userinfo=[]
 
 
 
+
+
+///////////////////////////////add signup data to local storage////////////////////
 signup_BTN.addEventListener("click", (e) => {
 
     saveusrData()
@@ -19,6 +25,21 @@ signup_BTN.addEventListener("click", (e) => {
     window.localStorage.setItem(mail.value,JSON.stringify(userinfo));    
   } 
 }
+
+///////////////////////////////get login data from local storage////////////////////
+//////////still testing phase//////////
+// login_BTN.addEventListener("click", (e) => {
+
+//     getData()
+//   });
+
+//   function getData(){
+//    for(let i=0;i<userinfo.length;i++)
+//    if(mailLogin.value===localStorage.getItem(mail.value))
+//    alert("you are a member in our community");
+  
+// }
+/////////////////////////////////////////
 
 
 
