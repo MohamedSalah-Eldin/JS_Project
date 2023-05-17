82a226dc1444323d9a9f8f170afe9a1fc3ca3b2b
MohamedSalah-Eldin <Mhmdabdelkhaliq12@gmail.com>
2023-05-17 11:45:50 +0300

    javascript

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,14 +1,24 @@
-let usrname=document.getElementById("username");
-let psswod=document.getElementById("password");
+let usrname=document.getElementById("usernameId");
+let psswod=document.getElementById("passwordId");
+let mail=document.getElementById("emailId");
 let signup_BTN=document.getElementById("signupBTN");
-
+let userinfo=[]
 
 
 
 signup_BTN.addEventListener("click", (e) => {
-    window.localStorage.setItem(usrname.id,usrname.value)
-    window.localStorage.setItem(psswod.id,psswod.value)
+
+    saveusrData()
   });
+
+  function saveusrData(){
+    if(window.localStorage.getItem(mail.value)===null && mail.value !=="")
+   {
+    userinfo.push(usrname.value);
+    userinfo.push(psswod.value);
+    window.localStorage.setItem(mail.value,JSON.stringify(userinfo));    
+  } 
+}
 
 
 
