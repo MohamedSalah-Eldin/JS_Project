5ca0d49a1e59b99817519f8e31e3bab9e0898f2b
eslamyasser <e.yasser997@gmail.com>
2023-05-20 08:18:06 +0300

    another try with js for user login issue

--- js/home.js
+++ js/home.js
@@ -1,24 +1,18 @@
-
-
 document.addEventListener("DOMContentLoaded", () => {
   let loginState = sessionStorage.getItem('Islogin');
   let LoginUserName = sessionStorage.getItem('LoginUserName')
   let signBTN = document.getElementById("userlink");
   let NavUsername = document.getElementById("userName");
 
-  
-
-  function xloading(){
-    if(loginState==true){
-      signBTN.innerHTML ='SignOut';
+  function xloading() {
+    if (Boolean(loginState)) {
+      signBTN.innerHTML = 'SignOut';
       NavUsername.innerHTML = LoginUserName;
-    }
-    else{
-      signBTN.innerHTML='SignIn';
-      NavUsername.innerHTML='';
+    } else {
+      signBTN.innerHTML = 'SignIn';
+      NavUsername.innerHTML = '';
     }
   }
-  object.addEventListener("load",xloading() );
+
+  window.addEventListener("load", xloading);
 });
-
-
