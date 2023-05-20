c064f1fc1ce88f6a2fa830eafff06f2f60028476
Mohamed Salah Eldin <mhmdabdelkhaliq12@gmail.com>
2023-05-20 21:42:01 +0300

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -1,7 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
+    
+    let AssignBTNVar = document.getElementById("AssignBTN");
+  let SETusername = document.getElementById("SETusernameId");
+    let SETemail = document.getElementById("SETemailId");
+    let SETpassword = document.getElementById("SETpasswordId");
+  
 
-
-
+    AssignBTNVar.addEventListener("click", SetUsrData);
+  
+    function SetUsrData() {
+      let userinfo = [];
+        userinfo.push(SETusername.value);
+        userinfo.push(SETpassword.value);
+        window.localStorage.setItem(SETemail.value, JSON.stringify(userinfo));
+    }
+  
 
 
     
